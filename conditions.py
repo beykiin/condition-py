7603b04bdd7894efdda4ac33e680e90691950f1b
Yasin Beken <yasinbeken16@gmail.com>
2024-03-03 13:54:06 +0300

    First Commit

---
+++ conditions.py
@@ -0,0 +1,21 @@
+# age=18
+# if age>18:
+#     print("You are adult!")
+# elif age<18:
+#     print("You are child!")
+# else:
+#     print("You are 18 years old!")
+
+# SONRADAN DOLDURUP HATA VERMESİNİ İSTEMİYORSAN 
+# age=18
+# if age>18:
+#     pass
+# print(age)
+
+age=25
+if age>18 and age<26:
+    print("You are adult!")
+elif age>26 or age==26:
+    print("You are child!")
+else:
+    print("You are 18 years old!")
